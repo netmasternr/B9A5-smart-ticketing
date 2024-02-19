4bfffb399a4cb8f494d111bdcbe078bcc650ebd6
netmasternr <netmasternr@gmail.com>
2024-02-19 19:05:20 +0600

    js update

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 
 const allCards = document.getElementsByClassName('cards');
-
 let sitCount = 0;
 
 for (const card of allCards) {
     card.addEventListener('click', function() {
-
-      // color
-      card.style.backgroundColor = 'green';
+        // color
+        card.style.backgroundColor = 'green';
 
         // One button click only
         card.removeEventListener('click', arguments.callee);
@@ -18,9 +16,12 @@ for (const card of allCards) {
         countDownValue -= 1;
         countDownElement.innerText = countDownValue;
 
-        
+        // create new Element
+         
 
-        //seats checking
+
+
+        // Seats checking
         if (sitCount >= 4) {
             alert('All seats are filled!');
             
@@ -29,23 +30,51 @@ for (const card of allCards) {
         }
 
         // Sit count up
-        sitCount += 1 ;
+        sitCount += 1;
         document.getElementById('sit-count').innerText = sitCount;
 
         // Total price calculation
         let total = 550;
         let calculate = total * sitCount;
+
         const totalPrice = document.getElementById('total-price');
         totalPrice.innerText = calculate;
 
         // Grant total calculation
         const grantTotal = document.getElementById('grant-total');
-        grantTotal.innerText = totalPrice.innerText;
-      //   console.log(grantTotal.innerText);
-
-
-
+        grantTotal.innerText = calculate;
     });
 }
 
+// Coupon area
+const btn = document.getElementById('coupon-btn');
 
+btn.addEventListener('click', function() {
+
+    const inputValue = document.getElementById('input').value;
+    const couponCodeA = 'NEW15';
+    const couponCodeB = 'Couple 20';
+
+    if (sitCount >= 4) {       
+        let grantTotal = parseInt(document.getElementById('grant-total').innerText);
+
+        if (inputValue === couponCodeA) {          
+            grantTotal *= 0.85; 
+
+        } else if (inputValue === couponCodeB) {            
+            grantTotal *= 0.8; 
+        }
+        
+        else {
+            alert('Invalid coupon code');
+            return;
+        }
+
+        // new grant total price
+        const grantTotalElement = document.getElementById('grant-total');
+        grantTotalElement.innerText = grantTotal;
+
+    } else {
+        alert('Please buy at least 4 tickets');
+    }
+});
